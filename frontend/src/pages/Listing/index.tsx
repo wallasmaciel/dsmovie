bc89d9b77187d35bb7c1265ca6396bb5ea2b5d07
Wallas Maciel <wallasmaciel919@gmail.com>
2022-03-13 13:31:20 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,61 +1,59 @@
-import React,  { Component, ReactNode } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import MovieCard from 'components/movieCard';
 import Pagination from 'components/pagination';
 import { BASE_URL } from 'utils/request';
+import iMoviePage from 'interfaces/iMoviePage';
 
-interface Props {}
-  
-interface State {}
-
-class Listing extends Component<Props, State> { 
-
-    constructor(props: Props) {
-        super(props);
-
-        // Forma errada
+const Listing: React.FC = () => { 
+    const [pageNumber, setPageNumber] = useState<number>(0);
+	
+    useEffect(() => {
+		// Forma errada
+        console.log('1 - ' + pageNumber);
         axios.get(`${BASE_URL}/movies?size=12&page=0`)
             .then(response => {
-                console.log(response);
+                const data = response.data as iMoviePage;
+                setPageNumber(data.number);
+
+                console.log('2 - ' + pageNumber);
             }); 
-    }
+    }, [ pageNumber ]);
 
-    render(): ReactNode {
-        return (
-            <>
-                <Pagination />
+    return (
+        <>
+            <Pagination />
 
-                <div className="container">
-                    <div className="row">
-                        <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                            <MovieCard />
-                        </div> 
-                        <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                            <MovieCard />
-                        </div> 
-                        <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                            <MovieCard />
-                        </div> 
-                        <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                            <MovieCard />
-                        </div> 
-                        <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                            <MovieCard />
-                        </div> 
-                        <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                            <MovieCard />
-                        </div> 
-                        <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                            <MovieCard />
-                        </div> 
-                        <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                            <MovieCard />
-                        </div> 
-                    </div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div> 
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div> 
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div> 
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div> 
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div> 
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div> 
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div> 
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div> 
                 </div>
-            </>
-        );
-    }
+            </div>
+        </>
+    ); 
 };
 
 export default Listing; 
