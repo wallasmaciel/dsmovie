8ef73ed19424a9f525a6546859d5d7c0570c20b2
Wallas Maciel <wallasmaciel919@gmail.com>
2022-03-13 13:49:04 -0300

    Props

--- frontend/src/components/movieCard/index.tsx
+++ frontend/src/components/movieCard/index.tsx
@@ -3,24 +3,21 @@ import { Link } from 'react-router-dom';
 import MovieScore from 'components/movieScore';
 import iMovie from 'interfaces/iMovie';
 
-class MovieCard extends Component {
-    private movie: iMovie = {
-        id: 1,
-        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
-        title: "The Witcher",
-        count: 2,
-        score: 4.5
-    };
+interface iProps {
+    movie: iMovie; 
+};
+
+class MovieCard extends Component<iProps> {
 
     render(): ReactNode {
         return (
             <div>
-                <img className="dsmovie-movie-card-image" src={ this.movie.image } alt={ this.movie.title } />
+                <img className="dsmovie-movie-card-image" src={ this.props.movie.image } alt={ this.props.movie.title } />
                 <div className="dsmovie-card-bottom-container">
-                    <h3>{ this.movie.title }</h3>
+                    <h3>{ this.props.movie.title }</h3>
                     <MovieScore />
 
-                    <Link to={`/form/${this.movie.id}`}>
+                    <Link to={`/form/${this.props.movie.id}`}>
                         <div className="btn btn-primary dsmovie-btn">Avaliar</div>
                     </Link>
                 </div>
--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -4,21 +4,37 @@ import MovieCard from 'components/movieCard';
 import Pagination from 'components/pagination';
 import { BASE_URL } from 'utils/request';
 import iMoviePage from 'interfaces/iMoviePage';
+import iMovie from 'interfaces/iMovie';
 
 const Listing: React.FC = () => { 
     const [pageNumber, setPageNumber] = useState<number>(0);
+    const [page, setPage] = useState<iMoviePage>({
+        content: [],
+        last: true,
+        totalPages: 0,
+        totalElements: 0,
+        size: 12, 
+        number: 0, 
+        first: true, 
+        numberOfElements: 0, 
+        empty: true
+    });
 	
     useEffect(() => {
-		// Forma errada
-        console.log('1 - ' + pageNumber);
-        axios.get(`${BASE_URL}/movies?size=12&page=0`)
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=title`)
             .then(response => {
                 const data = response.data as iMoviePage;
-                setPageNumber(data.number);
-
-                console.log('2 - ' + pageNumber);
+                setPage(data);
             }); 
     }, [ pageNumber ]);
+
+    const movie: iMovie = {
+        id: 1,
+        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
+        title: "The Witcher",
+        count: 2,
+        score: 4.5
+    };
 
     return (
         <>
@@ -26,30 +42,14 @@ const Listing: React.FC = () => {
 
             <div className="container">
                 <div className="row">
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div> 
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div> 
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div> 
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div> 
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div> 
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div> 
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div> 
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div> 
+                {
+                    page.content.map(movie => (
+                            <div key={ movie.id } className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                                <MovieCard movie={ movie }/>
+                            </div> 
+                        )
+                    )
+                }
                 </div>
             </div>
         </>
